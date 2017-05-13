a6249f88b0036613d3c903b047f3f2167be82161
Nikita Khristinin <nkhristinin@gmail.com>
2017-05-13 10:20:45 +0300

    Remove Header and add some styles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout'
-import Home from './Home'
+import CustomerHome from './CustomerHome'
 import CounterRoute from './Counter'
 
 /*  Note: Instead of using JSX, we recommend using react-router
@@ -9,7 +9,7 @@ import CounterRoute from './Counter'
 export const createRoutes = (store) => ({
   path        : '/',
   component   : CoreLayout,
-  indexRoute  : Home,
+  indexRoute  : CustomerHome,
   childRoutes : [
     CounterRoute(store)
   ]
