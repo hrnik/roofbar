8feefc36cd87cda80e7d15b9fadf29ac9bec5e4f
Nikita Khristinin <nkhristinin@gmail.com>
2017-05-28 15:03:18 +0300

    Big drink for available and disable

--- src/components/BigDrink/BigDrink.js
+++ src/components/BigDrink/BigDrink.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Button from 'components/Button'
+import { DRINK_STATUS_AVAILABLE } from 'store/bar'
+import classNames from 'classnames'
 
 import './BigDrink.scss'
 
-const BigDrink = ({ name, drinkID, description, img, makeOrder }) => {
+const BigDrink = ({
+  name,
+  drinkID,
+  description,
+  img,
+  makeOrder,
+  status,
+  disableDrink,
+  enableDrink,
+  disableMode = false
+}) => {
+  const isDisableDrink = status !== DRINK_STATUS_AVAILABLE
+  const editBtns = !isDisableDrink
+    ? <Button black onClick={() => disableDrink(drinkID)} fullWidth>Decline</Button>
+    : <Button onClick={() => enableDrink(drinkID)} fullWidth>Enable</Button>
+
   return (
     <div>
-      <div className='drink'>
+      <div className={classNames('drink', { 'drink--disable': isDisableDrink })}>
         <img className='drink__image' src={img} alt={name} />
         <div className='drink__text'>
           <div className='drink__name'>{name}</div>
           <div className='drink__description'>{description}</div>
         </div>
       </div>
-      <div className='btn-order-wraper'>
-        <Button onClick={() => makeOrder(drinkID)} fullWidth>Make order</Button>
+      <div className='btn-order-wraper' >
+        {!disableMode ? <Button disable={isDisableDrink} onClick={() => makeOrder(drinkID)} fullWidth>Make order</Button> : editBtns}
       </div>
     </div>
   )
