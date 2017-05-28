3d2f67f7f916ecc3dce2529149ecd563d5e390bb
Nikita Khristinin <nkhristinin@gmail.com>
2017-05-28 22:05:17 +0300

    Add custom dropdown

--- src/routes/Barmen/components/CommonBarmenView/CommonBarmenView.js
+++ src/routes/Barmen/components/CommonBarmenView/CommonBarmenView.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import './CommonBarmenView.scss'
+import Dropdown from 'react-dropdown'
+import { browserHistory } from 'react-router'
 
-export const CommonBarmenView = ({ children, location }) => (
-  <div className='customer-bar'>
-    <div className='customer-bar__main '>
-      <div className='container container--left barmen-view-top'>
-        <h2>{location.pathname}</h2>
-        <div className='select'>Bar is Open</div>
+const defaultRoutes = [{ value: '/barmen', label: 'Orders' }, { value: '/manage', label: 'Manage' }]
+
+
+export const CommonBarmenView = ({ children, location }) => {
+  const onSelect = option => {
+    browserHistory.push(option.value)
+  }
+  const options = defaultRoutes.filter(option => option.value !== location.pathname)
+  const placeholderOption = defaultRoutes.filter(option => option.value === location.pathname)[0] || defaultRoutes[0]
+  return (
+    <div className='customer-bar'>
+      <div className='customer-bar__main '>
+        <div className='container container--left barmen-view-top'>
+          <h2 className='Dropdown--simple'>
+            <Dropdown options={options} onChange={onSelect} placeholder={placeholderOption.label} className='Dropdown--simple' />
+          </h2>
+          <div className='select'>Bar is Open</div>
+        </div>
+        {children}
       </div>
-      {children}
     </div>
-  </div>
-)
+  )
+}
 
 export default CommonBarmenView
