39f67c5a62959b31386c0ecc96a046be4a231a7f
Nikita Khristinin <nikita.khristinin@exness.com>
2017-06-09 18:26:44 +0300

    Add some sw

--- src/main.js
+++ src/main.js
@@ -59,6 +59,22 @@ if (__DEV__) {
 
 
 
+//This is the "Offline page" service worker
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register sercie worker
+if (navigator.serviceWorker.controller) {
+  console.log('SW active service worker found, no need to register')
+} else {
+  //Register the ServiceWorker
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
+
+
+
 // ========================================================
 // Go!
 // ========================================================
