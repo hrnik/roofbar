55eae55d770a51fb72d54de0cb51ac9489364c94
Nikita Khristinin <nikita.khristinin@exness.com>
2017-06-09 11:45:28 +0300

    Add some styles

--- src/components/CategorySection/CategorySection.js
+++ src/components/CategorySection/CategorySection.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ListTitle from 'components/ListTitle'
 import Category from 'components/Category'
+import Slider from 'react-slick'
+import MediaQuery from 'react-responsive'
 
 import CocktailImage from './assets/Cocktail.svg'
 import JuiceImage from './assets/Juice.svg'
 import SmoothieImage from './assets/Smoothie.svg'
-
 
 import './CategorySection.scss'
 
@@ -13,32 +14,100 @@ const mapCategoryImage = {
   Alchocol: CocktailImage,
   Beer: CocktailImage,
   Juice: JuiceImage,
-  Smoothie: SmoothieImage,
+  Smoothie: SmoothieImage
 }
-const getImageForCategory = category => mapCategoryImage[category] || CocktailImage
 
-const CategorySection = ({ categories, numberSection, setActiveCategory, activeCategoryName, limits }) => {
+const getSliderSettings = length => ({
+  infinite: false,
+  centerPadding: '20px',
+  slidesToShow: length,
+  swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: length
+      }
+    },
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 6
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 5
+      }
+    }
+  ]
+})
+const getImageForCategory = category =>
+  mapCategoryImage[category] || CocktailImage
+
+const CategorySection = ({
+  categories,
+  numberSection,
+  setActiveCategory,
+  activeCategoryName,
+  limits
+}) => {
   return (
     <div className='list-section'>
-      <ListTitle number={numberSection} type='category' count={categories.length} />
-      <div className='category-list container'>
-        {categories.length > 0
-          ? categories.sort().map((category, index) => {
-            const limit = limits ? limits[category] : undefined
-            return (
-              <div data-index={index} key={index}>
-                <Category
-                  className='category-list__item'
-                  name={category}
-                  img={getImageForCategory(category)}
-                  limit={limit}
-                  setActiveCategory={setActiveCategory}
-                  active={activeCategoryName === category}
+      <ListTitle
+        number={numberSection}
+        type='category'
+        count={categories.length}
+      />
+      <div className=' container'>
+        <MediaQuery query='(max-width: 767px)'>
+          <div
+            onTouchMove={e => {
+              e.preventDefault()
+            }}
+            className='drinks-list--big'
+          >
+            {categories.length > 0
+              ? <Slider {...getSliderSettings(categories.length)}>
+                {categories.sort().map((category, index) => {
+                  const limit = limits ? limits[category] : undefined
+                  return (
+                    <div data-index={index} key={index}>
+                      <Category
+                        className='category-list__item'
+                        name={category}
+                        img={getImageForCategory(category)}
+                        limit={limit}
+                        setActiveCategory={setActiveCategory}
+                        active={activeCategoryName === category}
+                        />
+                    </div>
+                  )
+                })}
+              </Slider>
+              : null}
+          </div>
+        </MediaQuery>
+        <MediaQuery query='(min-width: 767px)'>
+          <div className='category-list'>
+            {categories.sort().map((category, index) => {
+              const limit = limits ? limits[category] : undefined
+              return (
+                <div data-index={index} key={index}>
+                  <Category
+                    className='category-list__item'
+                    name={category}
+                    img={getImageForCategory(category)}
+                    limit={limit}
+                    setActiveCategory={setActiveCategory}
+                    active={activeCategoryName === category}
                   />
-              </div>
-            )
-          })
-          : null}
+                </div>
+              )
+            })}
+          </div>
+        </MediaQuery>
       </div>
     </div>
   )
