3016af861b1906beb1eae700319aeffefb918b00
Nikita Khristinin <nikita.khristinin@exness.com>
2017-06-12 14:33:56 +0300

    Change link to facebook login

--- src/routes/Login/components/LoginView/LoginView.js
+++ src/routes/Login/components/LoginView/LoginView.js
@@ -10,7 +10,7 @@ const LoginView = ({ loginUser }) => {
   let host = 'http://localhost:3000'
   if (process.env.NODE_ENV === 'production') {
     id = '1707671159533847'
-    host = 'https://roofbar.surge.sh'
+    host = 'https://exness.bar'
   }
 
   const handleLogin = () => {
